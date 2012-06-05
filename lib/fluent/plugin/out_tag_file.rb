914274e189f28ee40ba9202a7f60b7a476c0fa6b
Yuku Takahashi <taka84u9@gmail.com>
2012-06-05 18:14:33 +0900

    bugfix

--- lib/fluent/plugin/out_tag_file.rb
+++ lib/fluent/plugin/out_tag_file.rb
@@ -6,32 +6,20 @@ module Fluent
       conf['buffer_path'] ||= File.join(conf['path'], 'buffer')
 
       super
-
-      @timesf = TimeFormatter.new(@time_slice_format, @localtime)
     end
 
-    # ==== Args
-    # tag    :: "PREFIX.path.to.dir"
-    # time   ::
-    # record ::
     def format(tag, time, record)
       tag_elems = tag.split('.')
       tag_elems.shift  # remove PREFIX
-      tag_elems.push(@timesf.format(time))
       dir = File.join(@path, *tag_elems)
 
       # @timef is assigned in FileOutput.configure
       time_str = @timef.format(time)
 
-      "#{dir}\t#{time_str}\t#{Yajl.dump(record)}"
+      [dir, "#{time_str}\t#{Yajl.dump(record)}\n"].to_msgpack
     end
 
     def write(chunk)
-      data = chunk.read
-      pos  = data.index("\t")
-      dir  = data[0...pos]
-      data = data[pos+1..-1] + "\n"
-
       case @compress
       when nil
         suffix = ''
@@ -39,18 +27,32 @@ module Fluent
         suffix = '.gz'
       end
 
-      i = 0
-      begin
-        path = File.join(dir, "#{i}.log#{suffix}")
-        i += 1
-      end while File.exist?(path)
-      FileUtils.mkdir_p dir
+      hash = {}
+      chunk.msgpack_each do |(dir, data)|
+        sym = dir.to_sym
+        if hash.include?(sym)
+          hash[sym] += data
+        else
+          hash[sym] = data
+        end
+      end
 
-      case @compress
-      when nil
-        File.open(path, 'a') {|f| f.write(data) }
-      when :gz
-        Zlib::GzipWriter.open(path) {|f| f.write(data) }
+      hash.each do |dir, data|
+        dir = File.join(dir.to_s, chunk.key)
+
+        i = 0
+        begin
+          path = File.join(dir, "#{i}.log#{suffix}")
+          i += 1
+        end while File.exist?(path)
+        FileUtils.mkdir_p dir
+
+        case @compress
+        when nil
+          File.open(path, 'a') {|f| f.write(data) }
+        when :gz
+          Zlib::GzipWriter.open(path) {|f| f.write(data) }
+        end
       end
     end
   end
