f605f853f34fa2ee5954f6d844e5bb966464aa47
Yuku Takahashi <taka84u9@gmail.com>
2012-06-05 16:39:31 +0900

    refactoring: path points log dir not buffer file

--- lib/fluent/plugin/out_tag_file.rb
+++ lib/fluent/plugin/out_tag_file.rb
@@ -2,22 +2,35 @@ module Fluent
   class TagFileOutput < FileOutput
     Plugin.register_output('tag_file', self)
 
+    def configure(conf)
+      conf['buffer_path'] ||= File.join(conf['path'], 'buffer')
+
+      super
+
+      @timesf = TimeFormatter.new(@time_slice_format, @localtime)
+    end
+
+    # ==== Args
+    # tag    :: "PREFIX.path.to.dir"
+    # time   ::
+    # record ::
+    def format(tag, time, record)
+      tag_elems = tag.split('.')
+      tag_elems.shift  # remove PREFIX
+      tag_elems.push(@timesf.format(time))
+      dir = File.join(@path, *tag_elems)
+
+      # @timef is assigned in FileOutput.configure
+      time_str = @timef.format(time)
+
+      "#{dir}\t#{time_str}\t#{Yajl.dump(record)}"
+    end
+
     def write(chunk)
       data = chunk.read
-
-      # tag :: "PREFIX.path.to.dir"
-      time_str, tag, record = data.split("\t")
-
-      elems = tag.split('.')
-      elems.shift  # remove prefix
-      elems.push(time_str)
-
-      # *Attention* @path points buffer dir not log directory
-      # @path    :: /var/log/fluent/buffer
-      # root_dir :: /var/log/fluent
-      # dir      :: /var/log/fluent/path/to/dir/%Y/%m/%d/%H
-      root_dir = @path.split('/')[0...-1]
-      dir = File.join(root_dir, *elems)
+      pos  = data.index("\t")
+      dir  = data[0...pos]
+      data = data[pos+1..-1] + "\n"
 
       case @compress
       when nil
@@ -28,7 +41,7 @@ module Fluent
 
       i = 0
       begin
-        path = File.join(dir, "#{i}#{@path_suffix}#{suffix}")
+        path = File.join(dir, "#{i}.log#{suffix}")
         i += 1
       end while File.exist?(path)
       FileUtils.mkdir_p dir
@@ -39,8 +52,6 @@ module Fluent
       when :gz
         Zlib::GzipWriter.open(path) {|f| f.write(data) }
       end
-
-      return path  # for test
     end
   end
 end
